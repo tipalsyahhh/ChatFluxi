790a5136a3e991467541e31f296e1fd9a0c68fc0
tipalsyahhh <tipalsyah123@gmail.com>
2024-10-05 09:34:24 +0700

    chat

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById("loader");
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 5000);
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 300000);
+});
